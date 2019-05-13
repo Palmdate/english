b7d63010c7cc9dc411c483c82b11f489e18158d1
Ha Nguyen <15913579+Palmdate@users.noreply.github.com>
2019-05-13 17:53:59 +0700

    Improve layout of mail sent to reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     user.send_password_reset if user
-    flash[:notice] = 'E-mail sent with password reset instructions.'
+    flash[:success] = 'E-mail sent with password reset instructions. Please check your email.'
     redirect_to new_session_path
   end
 
@@ -16,10 +16,10 @@ class PasswordResetsController < ApplicationController
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hour.ago
-      flash[:notice] = 'Password reset has expired'
+      flash[:danger] = 'Password reset has expired'
       redirect_to new_password_reset_path
     elsif @user.update(user_params)
-      flash[:notice] = 'Password has been reset!'
+      flash[:warning] = 'Password has been reset!'
       redirect_to new_session_path
     else
       render :edit
