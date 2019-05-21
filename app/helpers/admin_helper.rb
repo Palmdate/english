cb7f2f75be1a671343836dcebd3cc5fb4c7a8788
Ha Nguyen <15913579+Palmdate@users.noreply.github.com>
2019-05-21 14:02:30 +0700

    Improved func and front end admin report

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,14 +1,14 @@
 module AdminHelper
   def list_7_days(user_id)
-    list = ReadAloudChart.where("date(created_at) between :start and :end", {start: 7.days.ago.to_date, end: Date.today}).where(:user_id => user_id).order(:updated_at)
+    list = ReadAloudReport.where("date(updated_at) between :start and :end", {start: 7.days.ago.to_date, end: Date.today}).where(:user_id => user_id).order(:updated_at)
     length_list = list.count
     array = []
     if list != []
       days = list.first.updated_at.to_date
       
-      (0..7).each do |x|
+      (0..6).each do |x|
         
-        array = array + [["#{days + x.day}"] + list.select{|m| m.updated_at.to_date == days + x.day}.map{|k| k.rate}]
+        array = array + [["#{days + x.day}"] + list.select{|m| m.updated_at.to_date == days + x.day}.map{|k| k.percent}]
       end
     end
     # data = [['Year', 'Sales', 'Expenses', 'Profit'],
