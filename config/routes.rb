4daf9a75849d4f6ed73f4ee2d29194e3f6166d95
Ha Nguyen <15913579+Palmdate@users.noreply.github.com>
2019-05-23 23:11:03 +0700

    Handle error page

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,19 @@ Rails.application.routes.draw do
   post 'course/store'
   resources :course, only: [:edit, :update, :destroy]
   get 'home/course'
-  resources :users
+  resources :users, only: [:new]
   root "home#index"
   
   resources :sessions, only: [:new, :create, :destroy] 
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
-  resources :write_fr_dics, only:[:index, :public]
-  resources :password_resets
+  resources :write_fr_dics, only: [:index, :public]
+  resources :password_resets, only: [:new, :edit, :update]
 
   get 'write_fr_dics/public'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get '*path' => redirect('/404_customize')
+
 end
