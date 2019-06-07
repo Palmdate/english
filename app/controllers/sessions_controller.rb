1e12ca051e374718014660ad4d914b6d4e87c9b0
Ha Nguyen <15913579+Palmdate@users.noreply.github.com>
2019-06-07 15:03:17 +0700

    Always save email attribute with lowcase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    email_type = params[:email][0].downcase + params[:email][1..-1]
+    user = User.find_by_email(email_type)
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
