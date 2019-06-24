a4d2aed5dc1bae67aa4beb73cab631712a483a99
Ha Nguyen <15913579+Palmdate@users.noreply.github.com>
2019-06-24 16:23:03 +0700

    Refactor code in javascript file pronunciation. Aplly code by running: rails assets:clean; rails assets:precompile.

--- app/assets/javascripts/pronunciation.js
+++ app/assets/javascripts/pronunciation.js
@@ -1,99 +1,100 @@
 $(document).on('turbolinks:load', function() {
-    var timeRecord;
-    var word = "";
-    var idWord = "";
-    var result = document.getElementById('erea_Say');
-    function showInfoWork(e) {
-        word = document.getElementById(e.id).innerHTML;
-        responsiveVoice.speak(word, "UK English Female", { rate: 0.9 });
-        document.getElementById("word").innerHTML = word;
+  var timeRecord;
+  var word = "";
+  var idWord = "";
+  var result = document.getElementById('erea_Say');
+ 
+  function showInfoWork(e) {
+    word = document.getElementById(e.id).innerHTML;
+    responsiveVoice.speak(word, "UK English Female", { rate: 0.9 });
+    document.getElementById("word").innerHTML = word;
 
-        $("#" + idWord).removeClass("text-info");
-        $("#" + e.id).addClass("text-info");
+    $("#" + idWord).removeClass("text-info");
+    $("#" + e.id).addClass("text-info");
 
-        try {
-            document.getElementById("word-example").removeChild(document.getElementById("word-example").lastElementChild)
-        } catch (error) {
-            
-        }
-
-        if(e.id == "work_say")
-        {
-            var para = document.createElement("p");
-            var node = document.createTextNode('"She say love me but I know she do not."');
-            para.appendChild(node);
-            var element = document.getElementById("word-example");
-            element.appendChild(para);
-        }
-        else{
-            var para = document.createElement("p");
-            var node = document.createTextNode('"Eight days a week."');
-            para.appendChild(node);
-            var element = document.getElementById("word-example");
-            element.appendChild(para);
-        }
-
-        $("#infor_Result").removeClass("d-none");
-        $("#erea_word").removeClass("d-none");
-
-        document.getElementById("erea_Say").innerHTML = "";
-        $("#result").addClass("d-none");
-
-        idWord = e.id;
-    }
-    
-    function speakWord(){
-        timeRecord = 5;
-        if(word !== ""){
-            document.getElementById("erea_Say").innerHTML = "";
-            $("#result").addClass("d-none");
-            record();
-        }
-        else{
-            alert("The word don't choice!!!");
-        }
+    try {
+      document.getElementById("word-example").removeChild(document.getElementById("word-example").lastElementChild)
+    } catch (error) {
+      
     }
 
-    function record(){
-        startConverting();
-        start = setInterval(function () {
-            timeRecord --;
-            var rs = document.getElementById("erea_Say").innerHTML;
-            if(timeRecord == 0 || rs != ""){
-                $("#result").removeClass("d-none");
-                showResult();
-
-                stopConverting();
-                clearInterval(start);
-
-                if(rs === ""){
-                    document.getElementById("erea_Say").innerHTML = "None";
-                }
-            }
-        }, 1000);
+    if(e.id == "work_say")
+    {
+      var para = document.createElement("p");
+      var node = document.createTextNode('"She say love me but I know she do not."');
+      para.appendChild(node);
+      var element = document.getElementById("word-example");
+      element.appendChild(para);
+    }
+    else{
+      var para = document.createElement("p");
+      var node = document.createTextNode('"Eight days a week."');
+      para.appendChild(node);
+      var element = document.getElementById("word-example");
+      element.appendChild(para);
     }
 
-    function showResult(){
-        var ctx = document.getElementById('result').getContext('2d');
-        var accuracyChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            datasets: [{
-            data: [50],
-            backgroundColor: ['#57b0f3']
-            }],
-        },
-        options: {
-            cutoutPercentage: 50
+    $("#infor_Result").removeClass("d-none");
+    $("#erea_word").removeClass("d-none");
+
+    document.getElementById("erea_Say").innerHTML = "";
+    $("#result").addClass("d-none");
+
+    idWord = e.id;
+  }
+  
+  function speakWord(){
+    timeRecord = 5;
+    if(word !== ""){
+      document.getElementById("erea_Say").innerHTML = "";
+      $("#result").addClass("d-none");
+      record();
+    }
+    else{
+      alert("The word don't choice!!!");
+    }
+  }
+
+  function record(){
+    startConverting();
+    start = setInterval(function () {
+      timeRecord --;
+      var rs = document.getElementById("erea_Say").innerHTML;
+      if(timeRecord == 0 || rs != ""){
+        $("#result").removeClass("d-none");
+        showResult();
+
+        stopConverting();
+        clearInterval(start);
+
+        if(rs === ""){
+          document.getElementById("erea_Say").innerHTML = "None";
         }
-        });
-    }
+      }
+    }, 1000);
+  }
 
-    function reviewSpeak(id){
-        responsiveVoice.speak(document.getElementById("word").innerHTML, "UK English Female", { rate: 0.9 });
-    }
+  function showResult(){
+    var ctx = document.getElementById('result').getContext('2d');
+    var accuracyChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        datasets: [{
+          data: [50],
+          backgroundColor: ['#57b0f3']
+        }],
+      },
+      options: {
+        cutoutPercentage: 50
+      }
+    });
+  }
 
-    // Speech to text
+  function reviewSpeak(id){
+    responsiveVoice.speak(document.getElementById("word").innerHTML, "UK English Female", { rate: 0.9 });
+  }
+
+  // Speech to text
 
   function startConverting () {
     if('webkitSpeechRecognition' in window) {
@@ -141,9 +142,30 @@ $(document).on('turbolinks:load', function() {
     if('webkitSpeechRecognition' in window) {
       speechRecognizer.continuous = false;
     }
-      speechRecognizer.stop();
-     
+    speechRecognizer.stop();
+    
 
   };
   // ------------Speech to text
+  
+  // Click function handle
+
+  // Handle click function
+  document.addEventListener('click', function (event) {
+    switch(event.target.id) {
+    case 'work_say':
+    case 'work_eight':
+      {
+        showInfoWork(event.target);
+        break;
+      }
+    case 'word':
+      reviewSpeak(event.target.id);
+      break;
+    default:
+      speakWord();
+    }
+    
+  }, false);
+
 });
